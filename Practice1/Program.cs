d6ea66ce96b4a1dfac1e91e06cf9a243c50b35b5
Natali <n_sunberry@inbox.ru>
2022-09-08 21:38:16 +0300

    Задачи

---
+++ Practice1/Program.cs
@@ -0,0 +1,39 @@
+﻿// Найти произведение двух матриц
+int[,] matr = new int[2, 2];
+int[,] matr1 = new int[2, 2];
+
+for (int m = 0; m < matr.GetLength(0); m++)
+{
+    for (int n = 0; n < matr.GetLength(1); n++)
+    {
+        matr[m, n] = new Random().Next(1, 10);
+        Console.Write($"{matr[m, n]} ");
+    }
+    Console.WriteLine();
+}
+
+Console.WriteLine();
+
+for (int m = 0; m < matr1.GetLength(0); m++)
+{
+    for (int n = 0; n < matr1.GetLength(1); n++)
+    {
+        matr1[m, n] = new Random().Next(1, 10);
+        Console.Write($"{matr1[m, n]} ");
+    }
+    Console.WriteLine();
+}
+
+Console.WriteLine();
+
+int[,] compMatr = new int[2, 2];
+for (int m = 0; m < matr.GetLength(0); m++)
+{
+    for (int n = 0; n < matr.GetLength(1); n++)
+    {
+        compMatr[m, n] = matr[m, n] * matr1[m, n];
+    Console.Write($"{compMatr[m, n]} ");
+    }
+    Console.WriteLine();
+}
+
---
+++ Practice3/Program.cs
@@ -0,0 +1,52 @@
+﻿// Сформировать трехмерный массив не повторяющимися двузначными числами показать его построчно на экран выводя индексы соответствующего элемента
+Console.Clear();
+
+int[,,] matr = new int[3, 2, 3];
+
+for (int m = 0; m < matr.GetLength(0); m++)
+{
+    Console.WriteLine($"Page №: {m + 1} ");
+    for (int n = 0; n < matr.GetLength(1); n++)
+    {
+        for (int k = 0; k < matr.GetLength(2); k++)
+        {
+            matr[m, n, k] = new Random().Next(11, 99);
+            Console.Write($"Элемент: {matr[m, n, k]}");
+            Console.Write($" Индекс: {m},{n},{k}; ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[] temp = new int[matr.GetLength(0) * matr.GetLength(1) * matr.GetLength(2)];
+int num;
+for (int i = 0; i < temp.GetLength(0); i++)
+{
+    temp[i] = new Random().Next(10, 100);
+    num = temp[i];
+    if (i >= 1)
+    {
+        for (int j = 0; j < i; j++)
+        {
+            while (temp[i] == temp[j])
+            {
+                temp[i] = new Random().Next(10, 100);
+                j = 0;
+                num = temp[i];
+            }
+            num = temp[i];
+        }
+    }
+}
+int count = 0;
+for (int x = 0; x < matr.GetLength(0); x++)
+{
+    for (int y = 0; y < matr.GetLength(1); y++)
+    {
+        for (int z = 0; z < matr.GetLength(2); z++)
+        {
+            matr[x, y, z] = temp[count];
+            count++;
+        }
+    }
+}
